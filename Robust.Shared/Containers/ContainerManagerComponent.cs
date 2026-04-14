c583aa46424d7042fef3072a025f90378a30fb1f
deltanedas <39013340+deltanedas@users.noreply.github.com>
2026-04-14 20:31:19 +0100

    Revert "Fully obsolete methods in ContainerManagerComponent (#6477)" (#4)
    Co-authored-by: deltanedas <@deltanedas:kde.org>

--- Robust.Shared/Containers/ContainerManagerComponent.cs
+++ Robust.Shared/Containers/ContainerManagerComponent.cs
@@ -4,128 +4,142 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Robust.Shared.GameObjects;
 using Robust.Shared.GameStates;
+using Robust.Shared.IoC;
+using Robust.Shared.Map;
+using Robust.Shared.Maths;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.Manager.Attributes;
 
-namespace Robust.Shared.Containers;
-
-/// <summary>
-/// Holds data about a set of entity containers on this entity.
-/// </summary>
-[NetworkedComponent]
-[RegisterComponent, ComponentProtoName("ContainerContainer")]
-public sealed partial class ContainerManagerComponent : Component, ISerializationHooks
+namespace Robust.Shared.Containers
 {
     /// <summary>
-    /// Dictionary containing the containers on this entity.
-    /// The key is used as an identifier and can be freely chosen when a new container is added with <see cref="SharedContainerSystem.MakeContainer"/>.
+    /// Holds data about a set of entity containers on this entity.
     /// </summary>
-    [DataField]
-    public Dictionary<string, BaseContainer> Containers = new();
-
-    // Requires a custom serializer + copier to get rid of. Good luck
-    void ISerializationHooks.AfterDeserialization()
+    [NetworkedComponent]
+    [RegisterComponent, ComponentProtoName("ContainerContainer")]
+    public sealed partial class ContainerManagerComponent : Component, ISerializationHooks
     {
-        foreach (var (id, container) in Containers)
-        {
-            container.Init(default!, id, (Owner, this));
-        }
-    }
+        [Dependency] private readonly IEntityManager _entMan = default!;
 
-    [Serializable, NetSerializable]
-    internal sealed class ContainerManagerComponentState : ComponentState
-    {
-        public Dictionary<string, ContainerData> Containers;
+        [DataField("containers")]
+        public Dictionary<string, BaseContainer> Containers = new();
 
-        public ContainerManagerComponentState(Dictionary<string, ContainerData> containers)
+        // Requires a custom serializer + copier to get rid of. Good luck
+        void ISerializationHooks.AfterDeserialization()
         {
-            Containers = containers;
+            foreach (var (id, container) in Containers)
+            {
+                container.Init(default!, id, (Owner, this));
+            }
         }
+
+        [Obsolete]
+        public bool TryGetContainer(string id, [NotNullWhen(true)] out BaseContainer? container)
+            => _entMan.System<SharedContainerSystem>().TryGetContainer(Owner, id, out container, this);
+
+        [Obsolete]
+        public bool TryGetContainer(EntityUid entity, [NotNullWhen(true)] out BaseContainer? container)
+            => _entMan.System<SharedContainerSystem>().TryGetContainingContainer(Owner, entity, out container, this);
+
+        [Obsolete]
+        public AllContainersEnumerable GetAllContainers()
+            => _entMan.System<SharedContainerSystem>().GetAllContainers(Owner, this);
 
         [Serializable, NetSerializable]
-        public readonly struct ContainerData
+        internal sealed class ContainerManagerComponentState : ComponentState
         {
-            public readonly string ContainerType; // TODO remove this. We dont have to send a whole string.
-            public readonly bool ShowContents;
-            public readonly bool OccludesLight;
-            public readonly NetEntity[] ContainedEntities;
+            public Dictionary<string, ContainerData> Containers;
 
-            public ContainerData(string containerType, bool showContents, bool occludesLight, NetEntity[] containedEntities)
+            public ContainerManagerComponentState(Dictionary<string, ContainerData> containers)
             {
-                ContainerType = containerType;
-                ShowContents = showContents;
-                OccludesLight = occludesLight;
-                ContainedEntities = containedEntities;
+                Containers = containers;
             }
 
-            public void Deconstruct(out string type, out bool showEnts, out bool occludesLight, out NetEntity[] ents)
+            [Serializable, NetSerializable]
+            public readonly struct ContainerData
             {
-                type = ContainerType;
-                showEnts = ShowContents;
-                occludesLight = OccludesLight;
-                ents = ContainedEntities;
+                public readonly string ContainerType; // TODO remove this. We dont have to send a whole string.
+                public readonly bool ShowContents;
+                public readonly bool OccludesLight;
+                public readonly NetEntity[] ContainedEntities;
+
+                public ContainerData(string containerType, bool showContents, bool occludesLight, NetEntity[] containedEntities)
+                {
+                    ContainerType = containerType;
+                    ShowContents = showContents;
+                    OccludesLight = occludesLight;
+                    ContainedEntities = containedEntities;
+                }
+
+                public void Deconstruct(out string type, out bool showEnts, out bool occludesLight, out NetEntity[] ents)
+                {
+                    type = ContainerType;
+                    showEnts = ShowContents;
+                    occludesLight = OccludesLight;
+                    ents = ContainedEntities;
+                }
             }
         }
-    }
 
-    public readonly struct AllContainersEnumerable : IEnumerable<BaseContainer>
-    {
-        private readonly ContainerManagerComponent? _manager;
-
-        public AllContainersEnumerable(ContainerManagerComponent? manager)
+        public readonly struct AllContainersEnumerable : IEnumerable<BaseContainer>
         {
-            _manager = manager;
-        }
+            private readonly ContainerManagerComponent? _manager;
 
-        public AllContainersEnumerator GetEnumerator()
-        {
-            return new(_manager);
-        }
-
-        IEnumerator<BaseContainer> IEnumerable<BaseContainer>.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-    }
-
-    public struct AllContainersEnumerator : IEnumerator<BaseContainer>
-    {
-        private Dictionary<string, BaseContainer>.ValueCollection.Enumerator _enumerator;
-
-        public AllContainersEnumerator(ContainerManagerComponent? manager)
-        {
-            _enumerator = manager?.Containers.Values.GetEnumerator() ?? new();
-            Current = default;
-        }
-
-        public bool MoveNext()
-        {
-            while (_enumerator.MoveNext())
+            public AllContainersEnumerable(ContainerManagerComponent? manager)
             {
-                Current = _enumerator.Current;
-                return true;
+                _manager = manager;
             }
 
-            return false;
+            public AllContainersEnumerator GetEnumerator()
+            {
+                return new(_manager);
+            }
+
+            IEnumerator<BaseContainer> IEnumerable<BaseContainer>.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
         }
 
-        void IEnumerator.Reset()
+        public struct AllContainersEnumerator : IEnumerator<BaseContainer>
         {
-            ((IEnumerator<BaseContainer>) _enumerator).Reset();
-        }
+            private Dictionary<string, BaseContainer>.ValueCollection.Enumerator _enumerator;
 
-        [AllowNull]
-        public BaseContainer Current { get; private set; }
+            public AllContainersEnumerator(ContainerManagerComponent? manager)
+            {
+                _enumerator = manager?.Containers.Values.GetEnumerator() ?? new();
+                Current = default;
+            }
 
-        object IEnumerator.Current => Current;
+            public bool MoveNext()
+            {
+                while (_enumerator.MoveNext())
+                {
+                    Current = _enumerator.Current;
+                    return true;
+                }
 
-        public void Dispose()
-        {
+                return false;
+            }
+
+            void IEnumerator.Reset()
+            {
+                ((IEnumerator<BaseContainer>) _enumerator).Reset();
+            }
+
+            [AllowNull]
+            public BaseContainer Current { get; private set; }
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+            }
         }
     }
 }
