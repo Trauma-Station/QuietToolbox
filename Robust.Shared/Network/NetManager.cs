fcd6fefca5df4c4afe9a970cdca4e13f4621a646
deltanedas <@deltanedas:kde.org>
2026-05-02 00:40:50 +0100

    record bandwidth usage on release, wont affect launcher

--- Robust.Shared/Network/NetManager.cs
+++ Robust.Shared/Network/NetManager.cs
@@ -1010,16 +1010,12 @@ namespace Robust.Shared.Network
             var instance = (NetMessage) Activator.CreateInstance(type)!;
             instance.MsgChannel = channel;
 
-#if DEBUG
-
             if (!_bandwidthUsage.TryGetValue(type, out var bandwidth))
             {
                 bandwidth = 0;
             }
 
             _bandwidthUsage[type] = bandwidth + msg.LengthBytes;
-
-#endif
 
             try
             {
