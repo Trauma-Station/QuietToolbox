f01f353a360166efc148750c944327d48f53eb46
deltanedas <@deltanedas:goida.zip>
2026-06-16 13:59:53 +0100

    add engine field to build info

--- Robust.Server/ServerStatus/StatusHost.Handlers.cs
+++ Robust.Server/ServerStatus/StatusHost.Handlers.cs
@@ -134,6 +134,7 @@ namespace Robust.Server.ServerStatus
 
             return new JsonObject
             {
+                ["engine"] = "QuietToolbox", // if you want to change this you have custom Robust.Client anyway, so hardcoding is fine
                 ["engine_version"] = buildInfo.EngineVersion,
                 ["fork_id"] = buildInfo.ForkId,
                 ["version"] = buildInfo.Version,
@@ -160,6 +161,7 @@ namespace Robust.Server.ServerStatus
             }
             return new JsonObject
             {
+                ["engine"] = "QuietToolbox",
                 ["engine_version"] = _cfg.GetCVar(CVars.BuildEngineVersion),
                 ["fork_id"] = fork,
                 ["version"] = acm.ManifestHash,
