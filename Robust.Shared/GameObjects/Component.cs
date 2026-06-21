b631951b3f5552d36539eaecdc15e3a073caaccc
deltanedas <@deltanedas:goida.zip>
2026-06-21 06:48:45 +0100

    remove IComponent.Dirty

--- Robust.Shared/GameObjects/Component.cs
+++ Robust.Shared/GameObjects/Component.cs
@@ -103,14 +103,6 @@ namespace Robust.Shared.GameObjects
             set => LastModifiedTick = value;
         }
 
-        /// <inheritdoc />
-        [Obsolete]
-        public void Dirty(IEntityManager? entManager = null)
-        {
-            IoCManager.Resolve(ref entManager);
-            entManager.Dirty(Owner, this);
-        }
-
         // these two methods clear the LastModifiedTick/CreationTick to mark it as "not different from prototype load".
         // This is used as optimization in the game state system to avoid sending redundant component data.
         void IComponent.ClearTicks()
