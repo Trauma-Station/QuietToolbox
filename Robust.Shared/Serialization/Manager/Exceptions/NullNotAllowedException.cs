d51d34f6eae39ed0360e798dd2dcce8744ac9af1
deltanedas <@deltanedas:goida.zip>
2026-06-26 08:52:43 +0100

    fix client

--- Robust.Shared/Serialization/Manager/Exceptions/NullNotAllowedException.cs
+++ Robust.Shared/Serialization/Manager/Exceptions/NullNotAllowedException.cs
@@ -4,6 +4,8 @@ namespace Robust.Shared.Serialization.Manager.Exceptions;
 
 public sealed class NullNotAllowedException(string name) : Exception
 {
+    public NullNotAllowedException() : this("?") {}
+
     public readonly string Name = name;
 
     public override string Message => $"Null value provided for reading '{name}' but type was not nullable";
